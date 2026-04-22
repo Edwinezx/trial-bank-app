054d3a9982f9dc3bf9224dd331d040257e2b8aee
Chukwudubem Chikezie Ezue <you@email.com>
2026-04-22 15:28:50 +0100

    New security updates

--- trial-bank-app/src/main/java/com/edwin/trial_bank_app/securityConfig/SecurityConfig.java
+++ trial-bank-app/src/main/java/com/edwin/trial_bank_app/securityConfig/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.edwin.trial_bank_app.securityConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable()) // disable CSRF for Postman
+                .csrf(AbstractHttpConfigurer::disable) // disable CSRF for Postman
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // allow all requests
                 );
