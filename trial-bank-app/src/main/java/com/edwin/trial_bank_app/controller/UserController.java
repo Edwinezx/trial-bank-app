ae0dfb9b2c6f2bdb7c3fc6f34a4551b8179d98a3
Chukwudubem Chikezie Ezue <you@email.com>
2026-05-24 10:12:37 +0100

    Implemented constructor injection in the controller class

--- trial-bank-app/src/main/java/com/edwin/trial_bank_app/controller/UserController.java
+++ trial-bank-app/src/main/java/com/edwin/trial_bank_app/controller/UserController.java
@@ -3,16 +3,17 @@ package com.edwin.trial_bank_app.controller;
 
 import com.edwin.trial_bank_app.dto.*;
 import com.edwin.trial_bank_app.service.impl.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 
 public class UserController {
+     private final UserService userService;
 
-    @Autowired
-    UserService userService;
+     public UserController(UserService userService) {
+         this.userService = userService;
+     }
 
     @PostMapping("/register")
     public BankResponse createAccount(@RequestBody CreateAccountRequest request) {
